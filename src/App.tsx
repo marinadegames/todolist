949af798c11aac182233b97d3dc2d0610235b403
Marinade <marinadegames@gmail.com>
2021-12-06 21:47:51 +0300

    LESSON 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import './App.css';
+import {Todolist} from "./Todolist";
 
 function App() {
+
+    const task1 = [
+        {id:1, title:'HTML&CSS', isDone:true},
+        {id:2, title:'JS', isDone:false},
+        {id:3, title:'React', isDone:false},
+    ]
+    const task2 = [
+        {id:1, title:'HTML&CSS', isDone:true},
+        {id:2, title:'JS', isDone:true},
+        {id:3, title:'React', isDone:false},
+    ]
+
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <Todolist title={'What to learn'} task={task1}/>
+            <Todolist title={'What to drink'} task={task2}/>
         </div>
     );
 }
