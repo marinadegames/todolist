81b72c7569cb4baf92cc225b1dd2cad66425d93f
Marinade <marinadegames@gmail.com>
2021-12-13 21:34:50 +0300

    add hooks

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ import {Todolist} from './Todolist';
 
 function App() {
 
-    const [tasks, setState] = useState( [
-        { id: 1, title: "HTML&CSS", isDone: true },
-        { id: 2, title: "JS", isDone: true },
-        { id: 3, title: "ReactJS", isDone: false },
-        { id: 4, title: "Redux", isDone: true },
-        { id: 5, title: "Router-dom", isDone: true },
-        { id: 6, title: "Good!", isDone: false },
+    const [tasks, setState] = useState([
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "ReactJS", isDone: false},
+        {id: 4, title: "Redux", isDone: true},
+        {id: 5, title: "Router-dom", isDone: true},
+        {id: 6, title: "Good!", isDone: false},
     ])
 
     const [filterValue, setFilterValue] = useState('all')
@@ -18,24 +18,24 @@ function App() {
 
     let isDoneTrue = tasks;
 
-    if(filterValue === 'ACTIVE'){
-        isDoneTrue = tasks.filter( t => t.isDone)
-    }else if (filterValue === 'COMPLETED'){
-        isDoneTrue = tasks.filter( t => !t.isDone)
+    if (filterValue === 'ACTIVE') {
+        isDoneTrue = tasks.filter(t => t.isDone)
+    } else if (filterValue === 'COMPLETED') {
+        isDoneTrue = tasks.filter(t => !t.isDone)
     }
 
-    const filteredTasks = (value:string) => {
+    const filteredTasks = (value: string) => {
         setFilterValue(value)
     }
 
-    const removeTask = (id:number) => {
-        setState(tasks.filter( t => t.id !== id))
+    const removeTask = (id: number) => {
+        setState(tasks.filter(t => t.id !== id))
     }
 
     return (
         <div className="App">
             <Todolist title="What to learn"
-                      tasks={tasks}
+                      tasks={isDoneTrue}
                       removeTask={removeTask}
                       filteredTasks={filteredTasks}/>
         </div>
