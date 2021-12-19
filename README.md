fe0642012f0cc86e24973db4aad942da7bbe880e
Marinade <marinadegames@gmail.com>
2021-12-19 17:45:38 +0300

    refactor code

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This repository for my 'Todo list' application.
 #### Update: ####
 💡 Lesson `01` - create app | Add `task-array`   
 📦 Lesson `02` - add `hooks`  
-🆔 Lesson `03` - add `uuid` | added to ability to `add tasks`  
+🆔 Lesson `03` - add `uuid` | added to ability to `add tasks` | You can use press `Enter` for add tasks
 
 ### 🙃 Good luck for me! ###
 ### `🚀 vjuuuuuuhhhhhhhhh 🚀` ###
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -17,31 +17,47 @@ type PropsType = {
     tasks: Array<TaskType>
     removeTask: (id: string) => void
     filteredTasks: (value: FilterType) => void
-    addTask: (title:string) => void
+    addTask: (title: string) => void
 }
 
 // components
 export function Todolist(props: PropsType) {
 
-    const [newTaskTitle, setNewTaskTitle] =  useState('')
-
-
+    const [newTaskTitle, setNewTaskTitle] = useState('')
+    const onChangeHandler = (event: any) => {
+        setNewTaskTitle(event.currentTarget.value)
+    }
+    const addTaskFunction = () => {
+        props.addTask(newTaskTitle)
+        setNewTaskTitle('')
+    }
+    const onKeyPressTask = (event: any) => {
+        if (event.charCode === 13) {
+            addTaskFunction();
+        }
+    }
 
     return <div>
         <h3>{props.title}</h3>
         <div className={s.addTaskForm}>
             <input value={newTaskTitle}
-                   onChange={ (event) => {
-                       setNewTaskTitle(event.currentTarget.value)
+                   onKeyPress={onKeyPressTask}
+                   onChange={onChangeHandler}/>
 
-                   } }/>
-            <button onClick={() => {props.addTask(newTaskTitle)} } className={s.btnAddTask}>+</button>
+            <button onClick={addTaskFunction} className={s.btnAddTask}>
+                +
+            </button>
         </div>
+
         <ul className={s.tasks}>
             {props.tasks.map((t) => {
                 return (
-                    <li key={t.id} >
-                        <button onClick={() => props.removeTask(t.id)}> X </button>
+                    <li key={t.id}>
+                        <button onClick={() => {
+                            props.removeTask(t.id)
+                        }
+                        }> X
+                        </button>
                         <input type="checkbox" checked={t.isDone}/>
                         <span> {t.title} </span>
                     </li>
